cb8f07efd343ae7bf448929c61fffc6e3fc7789f
William Miguel <wmiguel4815@gmail.com>
2021-03-09 02:47:59 -0800

    New files added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './css/index.css';
+// import './css/nav.css';
+// import Dashboard from "./pages/Dashboard.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div id="app">Yo</div>
+
+
+      <header>
+        <h1>Dashboard</h1>
       </header>
+      <div id="dashboard">
+        <section>
+          <i class="bi bi-clipboard"></i>
+          <div>
+            <p>166</p>
+            <h2>Work Orders</h2>
+          </div>
+        </section>
+        <section>
+          <i class="bi bi-file-earmark-music"></i>
+          <div>
+            <p>3</p>
+            <h2>On Demand</h2>
+          </div>
+        </section>
+        <section>
+          <i class="bi bi-person"></i>
+          <div>
+            <p>5</p>
+            <h2>Adminstration</h2>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
